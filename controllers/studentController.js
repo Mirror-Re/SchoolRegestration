bbf77e7badfba0be466b2ea48aed1daca8c22ac2
zsma <zsma@fresnou.org>
2025-04-08 09:14:19 -0700

    figure out error w/ studentForm (line 15), slide 19

---
+++ controllers/studentController.js
@@ -0,0 +1,27 @@
+const {Student} = require('../models');
+
+//view all
+module.exports.viewAll = async function (req, res) {
+    const students = await Student.findAll();
+    res.render('student/view_all', {students});
+}
+
+//profile
+module.exports.viewProfile = async function (req, res) {
+    const student = await Student.findByPk(req.params.id);
+    res.render('student/profile', {student});
+}
+
+//render add
+
+//add
+
+//render edit
+module.exports.renderEditForm = async function (req, res) {
+    const student = await Student.findByPk(req.params.id);
+    res.render('student/edit', {student});
+}
+
+//edit
+
+//delete
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const courseController = require('../controllers/courseController.js')
+const courseController = require('../controllers/courseController.js');
+const studentController = require('../controllers/studentController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +9,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/courses', courseController.viewAll);
+router.get('/courses/profile/:id', courseController.viewProfile);
+router.get('/courses/edit/:id/', courseController.renderEditForm);
+router.post('/courses/edit/:id', courseController.updateCourse);
+router.get('/courses/add', courseController.renderAddForm);
+router.post('/courses/add', courseController.addCourse);
+router.get('/courses/delete/:id', courseController.deleteCourse);
+
+
+router.get('/students', studentController.viewAll);
+router.get('/students/profile/:id', studentController.viewProfile);
+router.get('/students/edit/:id', studentController.renderEditForm);
+
 
 module.exports = router;
