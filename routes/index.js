d406c5917074a318f6b2726b713e36c74aeb59ee
zsma <zsma@fresnou.org>
2025-04-10 09:24:21 -0700

    fix code, continue to slide 35 (?)

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,8 @@ router.post('/students/add', studentController.addStudent);
 router.get('/students/delete/:id', studentController.deleteStudent);
 
 router.post('/students/:studentId/enroll/', studentController.enrollStudent);
+router.get('/students/:studentId/removeCourse/:courseId', studentController.removeCourse);
+router.post('/courses/:courseId/enroll', courseController.enrollStudent);
 
 
 module.exports = router;
